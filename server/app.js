65f8f1beac323428096c189474ec809347e7ae01
Josh Vardey <jocelynvardey@gmail.com>
2018-03-09 10:12:28 +0100

    deployErr

--- server/app.js
+++ server/app.js
@@ -61,10 +61,9 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
+  console.log(err);
   res.status(err.status || 500);
   res.json(process.env.NODE_ENV === "production" ? "error" : err);
 });
